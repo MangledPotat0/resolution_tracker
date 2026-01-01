21d3f6b92df4248fcdd58c148f9d96399b992fc8
Dawith <cephalopodmaster@protonmail.com>
2026-01-01 13:45:58 -0500

    Network config added to server test script

--- servertest.ps1
+++ servertest.ps1
@@ -1,5 +1,7 @@
 docker run --rm -it -v ${PWD}:/app/workdir `
 	   -p 5001:5000 `
 	   -e PYTHONPATH=/app/workdir `
+	   --env-file .env `
+	   --network=pg-network `
 	   --name flask `
 	   python-work
