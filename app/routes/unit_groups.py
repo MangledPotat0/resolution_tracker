a0c47c06804705f5fdddb8e4423940f6e4546863
Dawith <cephalopodmaster@protonmail.com>
2026-01-04 18:18:51 -0500

    Unit group update workflow complete

--- app/routes/unit_groups.py
+++ app/routes/unit_groups.py
@@ -10,7 +10,8 @@ from flask import Blueprint, current_app, render_template, request, redirect, \
                   url_for
 
 # local module imports
-from app.db.unit_queries import get_all_unit_groups, insert_unit_group
+from app.db.unit_queries import delete_unit_group, get_all_unit_groups, \
+        get_unit_group, insert_unit_group, update_unit_group
 
 unit_groups_bp = Blueprint("unit_groups", __name__)
 
@@ -23,9 +24,9 @@ def unit_groups_action_router():
         case "view":
             return redirect(url_for("unit_groups.view_unit_groups"))
         case "update":
-            return redirect(url_for("unit_groups.update_unit_group"))
+            return redirect(url_for("unit_groups.update_unit_group_start"))
         case "delete":
-            return redirect(url_for("unit_groups.delete_unit_group"))
+            return redirect(url_for("unit_groups.delete_unit_group_start"))
         case _:
             return "Invalid action", 400
 
@@ -48,5 +49,77 @@ def view_unit_groups():
     conn = current_app.db
     groups = get_all_unit_groups(conn)
     return render_template("unit_groups/view.html", groups=groups)
+
+@unit_groups_bp.route("/update_start", methods=["GET"])
+def update_unit_group_start():
+    return manipulate_unit_group_start("update")
+
+@unit_groups_bp.route("/delete_start", methods=["GET"])
+def delete_unit_group_start():
+    return manipulate_unit_group_start("delete")
+
+def manipulate_unit_group_start(action):
+    conn = current_app.db
+    unit_groups = get_all_unit_groups(conn)
+
+    return render_template(
+        f"unit_groups/{action}.html",
+        unit_groups=unit_groups,
+        hx_get_url=f"/unit_groups/{action}/get_unit_groups",
+        hx_target="#unit-group-dropdown"
+    )
+
+@unit_groups_bp.route("/update/get_unit_groups")
+@unit_groups_bp.route("/delete/get_unit_groups")
+def get_unit_groups():
+    conn = current_app.db
+    unit_groups = get_all_unit_groups(conn)
+    workflow = "/".join(request.path.split("/")[:3])
+    match workflow:
+        case "/unit_groups/update":
+            hx_get_url=f"{workflow}/get_unit_group_form"
+            hx_target="#unit-group-update-form"
+        case "/unit_groups/delete":
+            hx_get_url=f"{workflow}/get_unit_group_form"
+            hx_target="#unit-group-delete-form"
+        case _:
+            raise ValueError(f"Unexpected worfklow: {workflow}")
+    print(hx_get_url)
+    return render_template("unit_groups/partials/unit_group_dropdown.html",
+                           unit_groups=unit_groups,
+                           hx_get_url=hx_get_url,
+                           hx_target=hx_target
+    )
+
+@unit_groups_bp.route("/update/get_unit_group_form")
+def get_unit_group_upate_form():
+    group_id = request.args.get("id")
+    conn = current_app.db
+    group = get_unit_group(conn, group_id)
+
+    return render_template("unit_groups/partials/unit_group_update_form.html",
+                           group=group)
+
+@unit_groups_bp.route("/delete/get_unit_group_form")
+def get_unit_group_delete_form():
+    group_id = request.args.get("id")
+    conn = current_app.db
+    group = get_unit_group(conn, group_id)
+
+    return render_template("unit_groups/partials/unit_group_delete_form.html",
+                           group=group)
+
+@unit_groups_bp.route("/update/submit", methods=["POST"])
+def update_unit_submit():
+    conn = current_app.db
+    group_id = request.form.get("id")
+    group_name = request.form.get("name")
+    update_unit_group(conn, group_id, group_name)
+
+    return render_template("unit_groups/update_result.html", name=group_name)
+
+@unit_groups_bp.route("/delete/submit", methods=["POST"])
+def delete_unit_submit():
+    pass
 
 # EOF
