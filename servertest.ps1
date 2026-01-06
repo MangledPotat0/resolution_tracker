e7131c223c631d55a47d1f5003272c24dba5cbdd
Dawith <cephalopodmaster@protonmail.com>
2026-01-06 02:00:00 -0500

    Updated test launch script

--- servertest.ps1
+++ servertest.ps1
@@ -1,7 +1,7 @@
 docker run --rm -it -v ${PWD}:/app/workdir `
-	   -p 5001:5000 `
 	   -e PYTHONPATH=/app/workdir `
 	   --env-file .env `
 	   --network=pg-network `
 	   --name flask `
+	   -p 5001:5001 `
 	   resolutionpy /bin/bash
